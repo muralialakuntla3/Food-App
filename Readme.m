759accb06653b723f027a692944a9c20ee33c355
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-08-31 01:37:01 -0700

    Update and rename Readme.txt to Readme.m

--- Readme.m
+++ Readme.m
@@ -13,14 +13,14 @@ Psd Created by: https://www.behance.net/gallery/32552907/Food-And-Restaurant-Psd
 
 
 
-Rights: 
+#Rights: 
 You are permitted to use the resources for any number of personal and commercial projects.
 You may modify the resources according to your requirements and include them into works, 
 such as websites, applications or other materials intended for sale. No attribution or 
 link back to this site is required, however any credit will be much appreciated.
 
 
-Prohibitions:
+#Prohibitions:
 You do not have the rights to redistribute, resell, lease, license, sublicense or offer 
 files downloaded from http://bootstrapthemes.co to any third party ìas isî or as a separate attachment 
 from any of your work. If you wish to promote my resources on your site, you must link back 
